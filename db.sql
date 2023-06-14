edbf65094a039e196a7dc2921eede3851d2db120
sergejsvoronins <svoronins@gmail.com>
2023-06-14 07:32:52 +0200

    db has been modified

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Värd: 127.0.0.1
--- Tid vid skapande: 12 jun 2023 kl 12:04
+-- Tid vid skapande: 14 jun 2023 kl 07:31
 -- Serverversion: 10.4.28-MariaDB
 -- PHP-version: 8.2.4
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL,
   `name` varchar(32) NOT NULL,
-  `creating_date` date NOT NULL
+  `creating_date` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -38,11 +38,11 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `name`, `creating_date`) VALUES
-(2, 'pants', '2023-05-29'),
-(3, 't-shirts', '2023-06-08'),
-(5, 'jackets', '2023-06-08'),
-(6, 'shoes', '2023-06-08'),
-(7, 'shirts', '2023-06-08');
+(2, 'pants', '2023-05-29 19:14:12'),
+(3, 't-shirts', '2023-06-08 19:14:12'),
+(5, 'jackets', '2023-06-08 19:14:12'),
+(6, 'shoes', '2023-06-08 19:14:12'),
+(7, 'shirts', '2023-06-08 19:14:12');
 
 -- --------------------------------------------------------
 
@@ -57,8 +57,8 @@ CREATE TABLE `products` (
   `category_id` int(11) NOT NULL,
   `price` int(11) NOT NULL,
   `seller_id` int(11) NOT NULL,
-  `creating_date` date NOT NULL,
-  `selling_date` date DEFAULT NULL
+  `creating_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `selling_date` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -66,10 +66,10 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `size_id`, `category_id`, `price`, `seller_id`, `creating_date`, `selling_date`) VALUES
-(4, 'Jeans', 1, 2, 300, 1, '2023-05-29', '2023-05-30'),
-(6, 'byxor', 1, 2, 250, 1, '2023-05-30', '2023-06-05'),
-(7, 'jeans', 1, 2, 100, 2, '2023-06-01', '2023-06-02'),
-(75, 'adidas t-shirt', 2, 3, 100, 3, '2023-06-12', '2023-06-12');
+(4, 'Jeans', 1, 2, 300, 1, '2023-05-29 19:14:12', '2023-06-13 19:14:12'),
+(6, 'byxor', 1, 2, 250, 1, '2023-05-30 19:14:12', NULL),
+(7, 'jeans', 1, 2, 100, 2, '2023-06-01 19:14:12', NULL),
+(83, 'adidas t-shirt', 2, 3, 100, 3, '2023-06-13 19:22:53', NULL);
 
 -- --------------------------------------------------------
 
@@ -83,7 +83,7 @@ CREATE TABLE `sellers` (
   `last_name` varchar(32) NOT NULL,
   `epost` varchar(32) NOT NULL,
   `mobile` varchar(13) NOT NULL,
-  `creating_date` date NOT NULL
+  `creating_date` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -91,12 +91,16 @@ CREATE TABLE `sellers` (
 --
 
 INSERT INTO `sellers` (`id`, `first_name`, `last_name`, `epost`, `mobile`, `creating_date`) VALUES
-(1, 'Sergejs', 'Voronins', 's@v.se', '0720801071', '2023-05-29'),
-(2, 'Lisa', 'Johnsson', 'l@j.se', '075835050', '2023-05-29'),
-(3, 'Carl', 'Sylvan', 'c@s.se', '0762030945', '2023-05-30'),
-(4, 'Marta', 'Bengtsson', 'm@b.se', '0720254100', '2023-05-31'),
-(7, 'Joanna', 'Wellington', 'j@w.se', '0720245800', '2023-06-02'),
-(36, 'Göran', 'Larsson', 'g@l.se', '0720245834', '2023-06-07');
+(1, 'Sergejs', 'Voronins', 's@v.se', '0720801071', '2023-05-29 12:21:32'),
+(2, 'Lisa', 'Johnsson', 'l@j.se', '075835050', '2023-05-29 12:21:32'),
+(3, 'Carl', 'Sylvan', 'c@s.se', '0762030945', '2023-05-30 12:21:32'),
+(4, 'Marta', 'Bengtsson', 'm@b.se', '0720254100', '2023-05-31 12:21:32'),
+(7, 'Joanna', 'Wellington', 'j@w.se', '0720245800', '2023-06-02 12:21:32'),
+(36, 'Göran', 'Larsson', 'g@l.se', '0720245834', '2023-06-07 12:21:32'),
+(85, 'Frej', 'Voronins', 'f@v.se', '08923244698', '2023-06-13 12:21:32'),
+(87, 'Olof', 'Matsson', 'o@m.se', '072536900', '2023-06-13 19:09:29'),
+(90, 'Frej', 'Voronins', 'f@v.se', '08923244698', '2023-06-13 19:21:08'),
+(91, 'Frej', 'Voronins', 'f@v.se', '08923244698', '2023-06-13 19:31:48');
 
 -- --------------------------------------------------------
 
@@ -108,7 +112,7 @@ CREATE TABLE `sizes` (
   `id` int(11) NOT NULL,
   `name` varchar(3) NOT NULL,
   `description` varchar(32) NOT NULL,
-  `creating_date` date NOT NULL
+  `creating_date` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -116,10 +120,11 @@ CREATE TABLE `sizes` (
 --
 
 INSERT INTO `sizes` (`id`, `name`, `description`, `creating_date`) VALUES
-(1, 'XS', 'extra small', '2023-05-29'),
-(2, 'S', 'small', '2023-06-08'),
-(3, 'M', 'medium', '2023-06-08'),
-(4, 'L', 'large', '2023-06-08');
+(1, 'XS', 'extra small', '2023-05-29 19:14:12'),
+(2, 'S', 'small', '2023-06-08 19:14:12'),
+(3, 'M', 'medium', '2023-06-08 19:14:12'),
+(4, 'L', 'large', '2023-06-08 19:14:12'),
+(5, 'XL', 'extra large', '2023-06-13 19:14:12');
 
 --
 -- Index för dumpade tabeller
@@ -166,19 +171,19 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT för tabell `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;
 
 --
 -- AUTO_INCREMENT för tabell `sellers`
 --
 ALTER TABLE `sellers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=92;
 
 --
 -- AUTO_INCREMENT för tabell `sizes`
 --
 ALTER TABLE `sizes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restriktioner för dumpade tabeller
