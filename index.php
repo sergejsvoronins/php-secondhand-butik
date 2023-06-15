20418e307defe0526b512dee530bd863d2479f52
sergejsvoronins <svoronins@gmail.com>
2023-06-15 13:43:51 +0200

    readme has been updated

--- index.php
+++ index.php
@@ -7,20 +7,15 @@ require "controllers/controller.php";
 header("Content-Type: application/json");
 $request = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
-// try {
-    $sellerModel = new SellerModel();
-    $productModel = new ProductModel();
-    $secondhandApi = new SecondhandApi();
-    $controler = new Controller($secondhandApi, $method);
-    $controler->addRoute("sellers", $sellerModel, "getAllSellers", "GET");
-    $controler->addRoute("sellers/", $sellerModel, "getOneSeller", "GET");
-    $controler->addRoute("sellers", $sellerModel, "addSeller", "POST");
-    $controler->addRoute("products", $productModel, "getAllProducts", "GET");
-    $controler->addRoute("products/", $productModel, "getOneProduct", "GET");
-    $controler->addRoute("products/", $productModel, "addSellingDate", "PUT");
-    $controler->addRoute("products", $productModel, "addProduct", "POST");
-    $controler->start($request);
-// } catch (\Throwable $th) {
-//     http_response_code(503);
-//     echo $th->getMessage();
-// }
+$sellerModel = new SellerModel();
+$productModel = new ProductModel();
+$secondhandApi = new SecondhandApi();
+$controler = new Controller($secondhandApi, $method);
+$controler->addRoute("sellers", $sellerModel, "getAllSellers", "GET");
+$controler->addRoute("sellers/", $sellerModel, "getOneSeller", "GET");
+$controler->addRoute("sellers", $sellerModel, "addSeller", "POST");
+$controler->addRoute("products", $productModel, "getAllProducts", "GET");
+$controler->addRoute("products/", $productModel, "getOneProduct", "GET");
+$controler->addRoute("products/", $productModel, "addSellingDate", "PUT");
+$controler->addRoute("products", $productModel, "addProduct", "POST");
+$controler->start($request);
